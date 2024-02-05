94dcab9df2b79b301bb8425683d58e5b0409d730
tobeesupreem <t.wintersteller06@gmail.com>
2024-02-05 17:28:20 +0100

    Login-Funktionen funktioniert jetzt auch mit verschiedenen Benutzer (admin, root...), auch bei fehlerhaften Login werden Ränder der TB und PB rot.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
+using System.Linq;
+using System.Windows.Threading;
 using MySql.Data.MySqlClient;
 
 namespace Spotify
@@ -17,27 +20,60 @@ namespace Spotify
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string connectionString = "datasource = 127.0.0.1; port = 3306; username = root; password = root; database = it-woche-2024";
+
         public MainWindow()
         {
-            InitializeComponent();
+           
+        InitializeComponent();
             
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-           
+            Lbl_Fehlermeldung.Visibility = Visibility.Hidden;
         }
 
 
         private void Btn_anmelden_Click(object sender, RoutedEventArgs e)
         {
+            // ZUM VERSTÄNDIS: Wir erstellen erst eine Verbindung zum Server wenn wir auf den Button klicken, er checkt ob admin admin (Benutzer, Password) vorhanden ist und blendet das Overlay aus wenn Benutzer und Passwort stimmen. Wenn Benutzer und Passwort nicht stimmen, wird dir das angezeigt.
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            
             string BenutzerEingegeben = TB_benutzer.Text.ToString();
             string PasswortEingegeben = PB_passwort.Password.ToString();
 
-            if (BenutzerEingegeben == "admin" && PasswortEingegeben == "admin")
+            string query = "SELECT Benutzername, Passwort FROM login WHERE Benutzername = @Benutzername AND Passwort = @Passwort";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            cmd.Parameters.AddWithValue("@Benutzername", BenutzerEingegeben);
+            cmd.Parameters.AddWithValue("@Passwort", PasswortEingegeben);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
             {
                 LoginCanvas.Visibility = Visibility.Hidden;
             }
+            else
+            {
+                Lbl_Fehlermeldung.Visibility = Visibility.Visible;
+
+                TB_benutzer.Clear();
+                TB_benutzer.BorderBrush = Brushes.Red;
+                TB_benutzer.BorderThickness = new Thickness(3);
+
+                PB_passwort.Clear();
+                PB_passwort.BorderBrush = Brushes.Red;
+                PB_passwort.BorderThickness = new Thickness(3);
+            }
+
+            reader.Close(); 
+            conn.Close();
+            // Nach den Befehl wird die Connection zur Login-Tabelle geschlossen, weil ja der Login erfolgreich war.
+
         }
     }
 }
