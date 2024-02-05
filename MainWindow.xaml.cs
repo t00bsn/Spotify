d341eea9b19716ed666d84ab082872ed59312ad6
Dushan Pocev <didi.pocev@gmail.com>
2024-02-05 12:58:37 +0100

    Verbindung mit der Tabelle Songs mit Visuell Studio

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Media;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+
+
 
 namespace Spotify
 {
@@ -22,6 +29,34 @@ namespace Spotify
             
         }
 
-        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer("TIGER_Reezy.wav");
+            player.Play();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "datasource = 127.0.0.1; port = 3306; username = root; password = root; database = it-woche-2024";
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * from songinformationen", conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Lb_Ausgabe.Items.Add(reader[1] + " " + reader[2]);
+            }
+
+            
+
+            reader.Close();
+            conn.Close();
+
+        }
     }
 }
