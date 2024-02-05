75d8ddd01ce3def62c68a4d2329ddf7530c4d5d9
tobeesupreem <t.wintersteller06@gmail.com>
2024-02-05 09:29:41 +0100

    Testcommit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Spotify
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello Didi");
+        }
     }
 }
