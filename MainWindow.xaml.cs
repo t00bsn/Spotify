06d9a11d992bcf381dcfe18b60f01147cac49049
Dushan Pocev <didi.pocev@gmail.com>
2024-02-25 14:31:41 +0100

    Lieblinsgssongs Hinzufügen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace Spotify
         System.Windows.DependencyProperty dp, Object value);
         List<songinformationen> songlisteReezy = new List<songinformationen>();
         List<songinformationen> songlisteTravis = new List<songinformationen>();
+        List<songinformationen> lieblingssongs = new List<songinformationen>();
+        List<songinformationen> songs = new List<songinformationen>();
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -67,7 +69,7 @@ namespace Spotify
             for (int i = 0; i < songlisteReezy.Count; i++)
             {
                 Lb_AusgabeReezy.Items.Add(songlisteReezy[i].TiteldesSongs + " ~ " + songlisteReezy[i].Künstler);
-                Lb_AusgabeReezy.FontSize = 30;
+                Lb_AusgabeReezy.FontSize = 24;
             }
 
 
@@ -155,7 +157,7 @@ namespace Spotify
             for (int i = 0; i < songlisteTravis.Count; i++)
             {
                 Lb_AusgabeTravis.Items.Add(songlisteTravis[i].TiteldesSongs + " ~ " + songlisteTravis[i].Künstler);
-                Lb_AusgabeTravis.FontSize = 30;
+                Lb_AusgabeTravis.FontSize = 24;
             }
         }
 
@@ -222,5 +224,79 @@ namespace Spotify
 
             Process(PB_SongsTravis);
         }
+
+        private void Img_Herz_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            // Den Pfad zum neuen Bild festlegen
+            string newImagePath = "heart_filled_icon_125259.png";
+
+            // Das Bild ändern
+            ChangeImage(newImagePath);
+
+
+            string connectionString = "datasource = 127.0.0.1; port = 3306; username = root; password = root; database = it-woche-2024";
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * from lieblingssongs", conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                lieblingssongs.Add(new songinformationen(Convert.ToInt32(reader[0]), reader[1].ToString(), reader[2].ToString(),
+                                                    reader[3].ToString(), Convert.ToDateTime(reader[4]), Convert.ToInt32(reader[5]),
+                                                    reader[6].ToString()));
+            }
+
+            /* for (int i = 0; i < lieblingssongs.Count; i++)
+             {
+                 Lb_AusgabeReezy.Items.Add(songlisteReezy[i].TiteldesSongs + " ~ " + songlisteReezy[i].Künstler);
+                 Lb_AusgabeReezy.FontSize = 24;
+             }*/
+
+            string query = "Insert Into lieblingssongs (Titel des Songs,Künstler/Interpret,Albumname,Erscheinungsjahr,Dauer,Song) Values (@Titel des Songs,@Künstler/Interpret,@Albumname,@Erscheinungsjahr,@Dauer,@Song)";
+
+
+            // Füge Parameter hinzu und setze ihre Werte
+            cmd.Parameters.AddWithValue("@Titel", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].TiteldesSongs);
+            cmd.Parameters.AddWithValue("@Interpret", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].Künstler);
+            cmd.Parameters.AddWithValue("@Albumname", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].Albumname);
+            cmd.Parameters.AddWithValue("@Erscheinungsjahr", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].Erscheinungsjahr);
+            cmd.Parameters.AddWithValue("@Dauer", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].DauerSEK);
+            cmd.Parameters.AddWithValue("@Song", songlisteReezy[Lb_AusgabeReezy.SelectedIndex].Song);
+
+                reader.Close();
+            conn.Close();
+
+
+
+
+
+
+        }
+
+        private void ChangeImage(string imagePath)
+        {
+            try
+            {
+                // Load the image from file
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(imagePath, UriKind.Relative);
+                bitmap.EndInit();
+
+                // Set the image control's source to the loaded image
+                Img_Herz.Source = bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading image: " + ex.Message);
+            }
+        }
+
+       
     }
 }
